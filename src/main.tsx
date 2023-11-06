5c737cb5085e84f26386fd9893f15d4d102a91f8
Patricia <patricia.femenias@gmail.com>
2023-11-06 19:58:18 +0100

    Add resets styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
