d0eecd51655c5bf211f7a8b45ed7301a2e8e12d0
Patricia <patricia.femenias@gmail.com>
2023-11-07 12:56:58 +0100

    Add MoviesList component

---
+++ src/components/MoviesList/MoviesList.tsx
@@ -0,0 +1,20 @@
+import { useAppSelector } from "../../store/hooks";
+import MovieCard from "../App/MovieCard/MovieCard";
+
+const MoviesList = (): React.ReactElement => {
+  const movies = useAppSelector((state) => {
+    return state.moviesState.movies;
+  });
+
+  return (
+    <ul className="movies-list">
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <MovieCard movie={movie} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MoviesList;
