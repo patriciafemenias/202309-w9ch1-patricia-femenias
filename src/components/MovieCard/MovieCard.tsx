2afc5e2ba5b6c87ecc1df8aa9ce13c80d201db82
Patricia <patricia.femenias@gmail.com>
2023-11-08 21:49:01 +0100

    Add toggleStatus function as dispatcher

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,13 +1,22 @@
+import { useCallback } from "react";
+import { useAppDispatch } from "../../store/hooks";
 import { MovieStructure } from "../../store/movies/types";
 import MovieCardStyled from "./MovieCardStyled";
+import { toggleWatchMovieActionCreator } from "../../store/movies/moviesSlice";
 
 interface MovieCardProps {
   movie: MovieStructure;
 }
 
 const MovieCard = ({
-  movie: { title, director, year, picture },
+  movie: { id, title, director, year, picture, isWatched },
 }: MovieCardProps): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  const toggleStatus = useCallback(() => {
+    dispatch(toggleWatchMovieActionCreator(id));
+  }, [dispatch, id]);
+
   return (
     <MovieCardStyled className="movie-card">
       <div className="movie-card__data">
@@ -25,6 +34,19 @@ const MovieCard = ({
           <dt className="movie-card__title-info">Year</dt>
           <dd className="movie-card__info">{year}</dd>
         </dl>
+        <label className="movie-card__checkbox">
+          Already seen?
+          <input
+            className="movie-card__checkbox__input"
+            type="checkbox"
+            name=""
+            id=""
+            onChange={toggleStatus}
+          />
+        </label>
+        <span className="movie-card__feedback">
+          {isWatched ? "🤢watched🤢" : ""}
+        </span>
       </div>
     </MovieCardStyled>
   );
