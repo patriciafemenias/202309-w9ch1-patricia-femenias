6e42c11a65543a7af30aafbdf915d507d077aee9
Patricia <patricia.femenias@gmail.com>
2023-11-08 19:15:46 +0100

    Add movieMock

---
+++ src/mocks/movieMock.ts
@@ -0,0 +1,11 @@
+import { MovieStructure } from "../store/movies/types";
+
+const movieMock: MovieStructure = {
+  id: 1,
+  title: "Brain Dead",
+  director: "Peter Jackson",
+  year: 1992,
+  picture: "https://i.ibb.co/r66TwSq/braindead.webp",
+};
+
+export default movieMock;
