437c4a57df3d06989fc7620b73a1ffc52d22f674
Patricia <patricia.femenias@gmail.com>
2023-11-09 22:48:13 +0100

    Add NewMoviePage test

---
+++ src/pages/NewMoviePage/NewMoviePage.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { store } from "../../store";
+import mainTheme from "../../styles/mainTheme";
+import NewMoviePage from "./NewMoviePage";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+describe("Given a Create page", () => {
+  describe("When it renders", () => {
+    test("Then it should show a heading with the text 'Add a new Serie B horror movie!'", () => {
+      const headingText = "Characters list";
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <ThemeProvider theme={mainTheme}>
+              <NewMoviePage />
+            </ThemeProvider>
+          </BrowserRouter>
+        </Provider>,
+      );
+
+      const heading = screen.getByRole("heading", {
+        name: headingText,
+      });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
