7053190916b9369476fd549e90d36f6bb3d16e82
Patricia <patricia.femenias@gmail.com>
2023-11-10 02:27:57 +0100

    Fix MovieForm classes

--- src/components/MovieForm/MovieForm.tsx
+++ src/components/MovieForm/MovieForm.tsx
@@ -1,32 +1,48 @@
+import MovieFormStyled from "./MovieFormStyled";
+
 const MovieForm = (): React.ReactElement => {
   return (
-    <form className="form" autoComplete="off">
+    <MovieFormStyled className="form" autoComplete="off">
       <div className="form__row">
-        <label htmlFor="title">Title:</label>
-        <input type="text" id="title" />
+        <label className="form__label" htmlFor="title">
+          Title:
+        </label>
+        <input className="form__input" type="text" id="title" />
       </div>
       <div className="form__row">
-        <label htmlFor="director">Director:</label>
-        <input type="text" id="director" />
+        <label className="form__label" htmlFor="director">
+          Director:
+        </label>
+        <input className="form__input" type="text" id="director" />
       </div>
       <div className="form__row">
-        <label htmlFor="year">Realased:</label>
-        <input type="number" />
+        <label className="form__label" htmlFor="year">
+          Realased:
+        </label>
+        <input className="form__input" type="number" />
       </div>
       <div className="form__row">
-        <label htmlFor="picture">Picture Url:</label>
-        <input type="url" id="picture" />
+        <label className="form__label" htmlFor="picture">
+          Picture Url:
+        </label>
+        <input className="form__input" type="url" id="picture" />
       </div>
-      <div className="form__row">
-        <label htmlFor="checkbox">Have you already seen it?</label>
-        <input type="checkbox" id="checkbox" />
+      <div className="form__row--checkbox">
+        <label className="form__label--checkbox" htmlFor="checkbox">
+          Have you already seen it?
+        </label>
+        <input
+          className="form__input--checkbox"
+          type="checkbox"
+          id="checkbox"
+        />
       </div>
-      <div className="form__button">
-        <button className="button" type="submit">
+      <div className="form__button--box">
+        <button className="form__button" type="submit">
           Create
         </button>
       </div>
-    </form>
+    </MovieFormStyled>
   );
 };
 
